cb10c8bfabb2768f2e0e9c60e58c3885c9efc48c
None <jiro1@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-08 01:31:15 +0000

    test

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'hello I am a developer my name is Ferhat',
         'environment': os.environ.get('ENVIRONMENT'),
         'ownner': 'jiro1',
         'namespace': os.environ.get('NAMESPACE')
     })
-@app.route('/soon')
+@app.route('/jiro1')
 def comming_soon():
     return jsonify({
-        'message': 'This is comming soon page!!'
+        'message': 'This is my page!!'
     })
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
