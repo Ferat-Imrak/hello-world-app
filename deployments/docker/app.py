0cbd818f7945d5c596feddf3e372e63814676917
None <jiro1@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-08 01:06:10 +0000

    hello

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -6,6 +6,7 @@ def hello_world():
     return jsonify({
         'message': 'hello world',
         'environment': os.environ.get('ENVIRONMENT'),
+        'ownner': 'jiro1',
         'namespace': os.environ.get('NAMESPACE')
     })
 @app.route('/soon')
